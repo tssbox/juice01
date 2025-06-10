84e432d2a3f057a553a45981448877c104dec95d
Viachaslau <slava@tssbox.com>
2025-06-10 15:05:58 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-file-upload.test.ts
@@ -0,0 +1,37 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /file-upload', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['file_upload', 'xxe', 'rfi', 'ssti', 'osi', 'xss'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/file-upload`,
+      body: {
+        error: 'File is not passed'
+      },
+      headers: { 'Content-Type': 'application/json' }
+    });
+});
---
+++ .brightsec/tests/post-snippets-verdict.test.ts
@@ -0,0 +1,38 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /snippets/verdict', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['bopla', 'sqli', 'xss', 'ssrf', 'lfi'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/snippets/verdict`,
+      body: {
+        selectedLines: [1, 2, 3],
+        key: 'exampleKey'
+      },
+      headers: { 'Content-Type': 'application/json' }
+    });
+});

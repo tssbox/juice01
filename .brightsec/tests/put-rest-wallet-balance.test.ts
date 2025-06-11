ab6e09cccbb3d6d73b25161a94e2d54ce2eee2b7
Viachaslau <slava@tssbox.com>
2025-06-11 13:51:25 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/put-rest-wallet-balance.test.ts
@@ -0,0 +1,40 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('PUT /rest/wallet/balance', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['bopla', 'sqli', 'csrf', 'id_enumeration', 'xss'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.PUT,
+      url: `${baseUrl}/rest/wallet/balance`,
+      body: {
+        UserId: 123,
+        paymentId: 456,
+        balance: 100
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});

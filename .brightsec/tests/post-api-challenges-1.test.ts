7138b45d0c5e42802ffad75bc47ebb51673e0c62
Viachaslau <slava@tssbox.com>
2025-06-21 00:13:50 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-challenges-1.test.ts
@@ -0,0 +1,51 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/challenges/1', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['sqli', 'csrf', 'xss', 'bopla', 'osi'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/Challenges/1`,
+      body: {
+        id: 1,
+        name: 'SQL Injection',
+        category: 'Injection',
+        description: 'Find and exploit SQL Injection vulnerabilities.',
+        difficulty: 3,
+        hint: 'Check for SQL syntax errors.',
+        hintUrl: null,
+        mitigationUrl: null,
+        key: 'sqli',
+        disabledEnv: null,
+        tutorialOrder: null,
+        tags: 'security,sql',
+        solved: false,
+        codingChallengeStatus: 0
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});

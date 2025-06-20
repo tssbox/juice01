b3c2a2985e0b7fa8e961e56babc3bb232f9d493c
Viachaslau <slava@tssbox.com>
2025-06-20 19:53:17 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-challenges.test.ts
@@ -0,0 +1,51 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/challenges', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'bopla', 'xss', 'sqli', 'id_enumeration', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/Challenges`,
+      body: {
+        id: 1,
+        name: 'Sample Challenge',
+        category: 'Sample Category',
+        description: 'This is a sample challenge description.',
+        difficulty: 3,
+        hint: 'Try harder!',
+        hintUrl: 'http://example.com/hint',
+        mitigationUrl: 'http://example.com/mitigation',
+        key: 'sampleKey',
+        disabledEnv: null,
+        tutorialOrder: 1,
+        tags: 'sample,challenge',
+        solved: false,
+        codingChallengeStatus: 0
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});

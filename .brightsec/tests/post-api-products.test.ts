9afe75446d8ee1d82543096b9af9b948b084cadd
Viachaslau <slava@tssbox.com>
2025-07-16 13:54:33 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-products.test.ts
@@ -0,0 +1,43 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/products', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['bopla', 'csrf', 'sqli', 'xss', 'file_upload'],
+      attackParamLocations: [AttackParamLocation.BODY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/Products`,
+      body: {
+        id: 1,
+        name: 'Apple Juice',
+        description: 'Freshly squeezed apple juice',
+        price: 2.99,
+        deluxePrice: 3.99,
+        image: 'apple-juice.png'
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});

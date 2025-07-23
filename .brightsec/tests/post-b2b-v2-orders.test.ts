55294ebfc7cb0e7bc2343376fbca550cfc37c15d
Viachaslau <slava@tssbox.com>
2025-07-23 13:14:02 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-b2b-v2-orders.test.ts
@@ -0,0 +1,49 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /b2b/v2/orders', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['osi', 'ssti', 'rfi', 'sqli', 'xss', 'csrf', 'bopla', 'business_constraint_bypass'],
+      attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.HEADER]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/b2b/v2/orders`,
+      body: {
+        cid: 'JS0815DE',
+        orderLines: [
+          {
+            productId: 8,
+            quantity: 500,
+            customerReference: 'PO0000001'
+          }
+        ],
+        orderLinesData: '[{"productId":12,"quantity":10000,"customerReference":["PO0000001.2", "SM20180105|042"],"couponCode":"pes[Bh.u*t"},{"productId":13,"quantity":2000,"customerReference":"PO0000003.4"}]'
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer <your-token-here>'
+      },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
